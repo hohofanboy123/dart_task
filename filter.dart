dc70bfba7d9a44299292dcc3dd67b80bbaeec6f7
Hamza Masuadi <92558518+hohofanboy123@users.noreply.github.com>
2023-10-26 23:01:21 +0300

    Update filter.dart

--- filter.dart
+++ filter.dart
@@ -1,4 +1,17 @@
+
+List<int> filter(List<int> list, bool Function(int) predicate) {
+  
+  final result = <int>[];
+  for (final item in list) {
+    if (predicate(item)) {
+      result.add(item);
+    }
+  }
+  return result;
+}
+
 void main() {
+  
   final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   final evenNumbers = filter(numbers, (number) => number % 2 == 0);
@@ -6,14 +19,4 @@ void main() {
 
   print('Even Numbers: $evenNumbers');
   print('Odd Numbers: $oddNumbers');
-}
-
-List<T> filter<T>(List<T> list, bool Function(T) predicate) {
-  final result = <T>[];
-  for (final item in list) {
-    if (predicate(item)) {
-      result.add(item);
-    }
-  }
-  return result;
 }
